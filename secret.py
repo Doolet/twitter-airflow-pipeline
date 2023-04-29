d09e4ebb74fc1585e78b756cd2809c91a4e7d408
doolet <daulet.medeubayev@gmail.com>
2023-04-30 02:13:09 +0600

    Added secret file

---
+++ secret.py
@@ -0,0 +1,4 @@
+access_key = '754797706619609088-idPgrGVgwhw8P9JUEpauWXgOOgwBRYP'
+access_secret = 'G2BK7UjqOQGLxTafpE7jxUBlPcW63eDGcWKDjT01StWMG'
+consumer_key = 'b6Lmp2bzhKuinz3kwXFw2P9kY'
+consumer_secret = 'utiwboL0gDQw3CLDxKOxb6WXDseSVG47zUl06UxIe1lUY9JLsz'
---
+++ twitter_etl.py
@@ -0,0 +1,8 @@
+import tweepy 
+import pandas as pd
+import json
+from datetime import datetime
+import s3fs
+import secret 
+
+access_key = 
