7af70edfd3f805ee7f80c922f3aca12d839c1e37
doolet <daulet.medeubayev@gmail.com>
2023-04-30 02:37:36 +0600

    Regenerated keys

--- twitter_etl.py
+++ twitter_etl.py
@@ -1,8 +1,28 @@
-import tweepy 
+# %%
+import tweepy as t
 import pandas as pd
 import json
 from datetime import datetime
 import s3fs
-import secret 
+import secret as s
 
-access_key = 
+access_key = s.access_key
+access_secret = s.access_secret
+consumer_key = s.consumer_key
+consumer_secret = s.consumer_secret
+
+# Twitter authentication
+auth = t.OAuthHandler(access_key, access_secret)
+auth.set_access_token(consumer_key, consumer_secret)
+
+# Create an API object
+api = t.API(auth)
+
+tweets = api.user_timeline(screen_name='@elonmusk',
+                           # 200 is the maximum allowed count
+                           count=200,
+                           include_rts = False,
+                           # Keep full_text
+                           tweet_mode = 'extended'
+                           )
+print(tweets)
